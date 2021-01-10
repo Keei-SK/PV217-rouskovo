6200a4fa882a7f842b1921652f291a5f9420b131
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 00:06:49 +0100

    added service method declarations

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
@@ -1,6 +1,10 @@
 package cz.muni.fi.pv217.rouskovo;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,7 +14,35 @@ public class AccountService {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String welcome() {
+        return "";
     }
+
+    @POST
+    //@RolesAllowed({"CUSTOMER", "ADMIN"})
+    @Path("/info")
+    @Transactional
+    public String setDetails(Customer customer) {
+
+        customer.username = "user"; // will be obtained from token
+        customer.persist();
+        return "Account information updated successfully";
+    }
+
+    @GET
+    //@RolesAllowed({"CUSTOMER", "ADMIN"})
+    @Path("/info")
+    public String showDetails() {
+
+        return "Show account details";
+    }
+
+    @GET
+    //@RolesAllowed({"CUSTOMER", "ADMIN"})
+    @Path("/myorders")
+    public String showOrders() {
+
+        return "Show account orders";
+    }
+
 }
