69dcb1082fd150566e765db54bbd1e224dd811ef
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-10 20:50:00 +0100

    added dependencies and simple test

--- rouskovo/token-generator-service/src/main/java/cz/muni/fi/pv217/rouskovo/TokenGenerator.java
+++ rouskovo/token-generator-service/src/main/java/cz/muni/fi/pv217/rouskovo/TokenGenerator.java
@@ -3,7 +3,7 @@ package cz.muni.fi.pv217.rouskovo;
 import io.smallrye.jwt.build.Jwt;
 
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,19 +13,23 @@ import java.util.HashSet;
 @Path("/token")
 public class TokenGenerator {
 
-    @GET
+    @POST
     @RolesAllowed("ADMIN")
+    @Path("/customer")
+    @Produces(MediaType.TEXT_PLAIN)
     public String generateCustomerToken(String upn) {
-        return generateToken(upn, new HashSet<>(Arrays.asList("CUSTOMER"));
+        return generateToken(upn, new HashSet<>(Arrays.asList("CUSTOMER")));
     }
 
-    @GET
+    @POST
+    @Path("/admin")
     @RolesAllowed("ADMIN")
+    @Produces(MediaType.TEXT_PLAIN)
     public String generateAdminToken(String upn) {
         return generateToken(upn, new HashSet<>(Arrays.asList("ADMIN", "CUSTOMER")));
     }
 
-    public String generateToken(String upn, HashSet<> groups) {
+    public String generateToken(String upn, HashSet<String> groups) {
         String token =
                 Jwt.issuer("https://example.com/issuer")
                         .upn(upn)
--- rouskovo/token-generator-service/src/test/java/cz/muni/fi/pv217/rouskovo/TokenGeneratorTest.java
+++ rouskovo/token-generator-service/src/test/java/cz/muni/fi/pv217/rouskovo/TokenGeneratorTest.java
@@ -1,7 +1,12 @@
 package cz.muni.fi.pv217.rouskovo;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.Test;
+
+import javax.json.Json;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,16 +14,35 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class TokenGeneratorTest {
 
-    /*
     @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/token")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+    @TestSecurity(user = "admin", roles = { "ADMIN" })
+    public void testTokenGenerator() {
+        String json = Json.createObjectBuilder()
+                .add("upn", "admin")
+                .build()
+                .toString();
+
+        var response = given()
+                .contentType(String.valueOf(ContentType.APPLICATION_JSON))
+                .filter(ResponseLoggingFilter.logResponseIfStatusCodeIs(200))
+                .body(json)
+                .when().post("/token/admin")
+                .then()
+                .statusCode(200);
+        // System.out.println(response.body.toString());
     }
 
-     */
+    /*
+    @Test
+    @TestSecurity(user = "admin", roles = { "ADMIN" })
+    public void testTokenGenerator() {
+        var response = given()
+                .when().get("/token")
+                .then()
+                .statusCode(200)
+                .extract();
+        System.out.println(response.toString());
+    }
+    */
 
 }
