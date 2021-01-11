8bf5dd403810e33be86408372a3f9452cd722890
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 19:59:15 +0100

    added openapi compatibility

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
@@ -1,6 +1,14 @@
 package cz.muni.fi.pv217.rouskovo;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.tags.Tags;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
@@ -14,44 +22,67 @@ import javax.ws.rs.core.SecurityContext;
 
 @Path("/account")
 @RequestScoped
+@RolesAllowed({"CUSTOMER", "ADMIN"})
+@Tags(value = @Tag(name = "Customer Account"))
+@SecurityRequirement(name = "jwt", scopes = {"global"})
 public class AccountService {
 
     @Inject
     private JsonWebToken jwt;
 
     @GET
-    @RolesAllowed({"CUSTOMER", "ADMIN"})
     @Path("/info")
+    @SecurityRequirement(name = "jwt", scopes = {})
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Current account information", content = @Content(schema = @Schema(implementation = Customer.class)))})
+    @Operation(summary = "View account information")
     @Produces(MediaType.TEXT_PLAIN)
-    public String showDetails(@Context SecurityContext ctx) {
+    public String viewCustomerDetails(@Context SecurityContext ctx) {
         Customer c = Customer.findByUsername(getUsername(ctx));
         return "Account details\n" + c.toString();
     }
 
     @POST
-    @RolesAllowed({"CUSTOMER", "ADMIN"})
     @Path("/info")
     @Transactional
-    public String setDetails(@Context SecurityContext ctx, Customer customer) {
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Successfully set account information", content = @Content(schema = @Schema(implementation = Customer.class)))})
+    @Operation(summary = "Set account information")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String setCustomerDetails(@Context SecurityContext ctx, Customer customer) {
 
         customer.username = getUsername(ctx);
         customer.persist();
         return "Account information updated successfully";
     }
 
-    @DELETE
-    @RolesAllowed({"CUSTOMER", "ADMIN"})
+    @PUT
     @Path("/info")
     @Transactional
-    public String deleteUser(@Context SecurityContext ctx, Customer custome) {
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Successfully Updated account information", content = @Content(schema = @Schema(implementation = Customer.class)))})
+    @Operation(summary = "Update account information")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String updateCustomer(@Context SecurityContext ctx, Customer customer) {
         Customer c = Customer.findByUsername(getUsername(ctx));
-
-        return "Your account info has been successfully deleted";
+        // update
+        return "Account information has been successfully updated";
     }
 
 
+    @DELETE
+    @Path("/info")
+    @Transactional
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Succesfully deleted account", content = @Content(schema = @Schema(implementation = Customer.class)))})
+    @Operation(summary = "Deletes customer's account")
+    public String deleteCustomer(@Context SecurityContext ctx, Customer custome) {
+        Customer c = Customer.findByUsername(getUsername(ctx));
+        c.delete();
+        return "Your account info has been successfully deleted";
+    }
+
     @GET
-    @RolesAllowed({"CUSTOMER", "ADMIN"})
     @Path("/myorders")
     public String showOrders() {
         return "Show account orders";
