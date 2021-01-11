1acaa8b8b28066ee3de36a3db6cdea0c898e3959
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 20:06:37 +0100

    added jwt security scheme

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
@@ -2,11 +2,13 @@ package cz.muni.fi.pv217.rouskovo;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.openapi.annotations.tags.Tags;
 
@@ -24,7 +26,7 @@ import javax.ws.rs.core.SecurityContext;
 @RequestScoped
 @RolesAllowed({"CUSTOMER", "ADMIN"})
 @Tags(value = @Tag(name = "Customer Account"))
-@SecurityRequirement(name = "jwt", scopes = {"global"})
+@SecurityScheme(securitySchemeName = "jwt", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "jwt")
 public class AccountService {
 
     @Inject
