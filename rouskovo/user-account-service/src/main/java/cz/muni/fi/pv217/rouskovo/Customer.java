a88ccb3634254128cf5083fff3c0ff31046ef474
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 01:33:48 +0100

    added dependency, minor customer entity fix

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/Customer.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/Customer.java
@@ -3,8 +3,10 @@ package cz.muni.fi.pv217.rouskovo;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.json.bind.annotation.JsonbProperty;
+import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 
+@Entity
 public class Customer extends PanacheEntity {
     @PrimaryKeyJoinColumn
     @JsonbProperty(nillable = true)
@@ -12,4 +14,6 @@ public class Customer extends PanacheEntity {
     public String address;
     public String phone;
     public String email;
+
+    public Customer() {}
 }
