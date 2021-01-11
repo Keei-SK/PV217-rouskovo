80fedd53ba29497ab3cdcff84f323826eb35cb54
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 14:18:51 +0100

    updated entity structure

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/Customer.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/Customer.java
@@ -11,9 +11,31 @@ public class Customer extends PanacheEntity {
     @PrimaryKeyJoinColumn
     @JsonbProperty(nillable = true)
     public String username;
+    public String name;
     public String address;
     public String phone;
     public String email;
 
     public Customer() {}
+
+    public Customer(String username, String name, String address, String phone, String email) {
+        this.username = username;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public static Customer findByUsername(String username) {
+        return find("username", username).firstResult();
+    }
+
+    @Override
+    public String toString() {
+        return "username: " + username + "\n" +
+                "name: " + name + "\n" +
+                "address: " + address + "\n" +
+                "phone: " + phone + "\n" +
+                "email: " + email + "\n";
+    }
 }
