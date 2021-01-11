54d2922ca3cdc397af5574c1f834e993595e57c3
Juraj Smeriga <jsmeriga@mail.muni.cz>
2021-01-11 01:46:35 +0100

    added partial jwt authentication functionality

--- rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
+++ rouskovo/user-account-service/src/main/java/cz/muni/fi/pv217/rouskovo/AccountService.java
@@ -1,28 +1,32 @@
 package cz.muni.fi.pv217.rouskovo;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("/account")
+@RequestScoped
 public class AccountService {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String welcome() {
-        return "";
-    }
+    @Inject
+    private JsonWebToken jwt;
 
     @POST
-    //@RolesAllowed({"CUSTOMER", "ADMIN"})
+    @RolesAllowed({"CUSTOMER", "ADMIN"})
     @Path("/info")
     @Transactional
-    public String setDetails(Customer customer) {
+    public String setDetails(@Context SecurityContext ctx, Customer customer) {
 
         customer.username = "user"; // will be obtained from token
         customer.persist();
@@ -33,7 +37,6 @@ public class AccountService {
     //@RolesAllowed({"CUSTOMER", "ADMIN"})
     @Path("/info")
     public String showDetails() {
-
         return "Show account details";
     }
 
